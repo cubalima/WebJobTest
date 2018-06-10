1806a48b70b44ff5ea59e44b497dbf45b6c9d596
Angel <alima@aidacanarias.com>
2018-06-10 19:20:25 +0100

    initialize web job project

---
+++ WebJob/Jobs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+
+namespace WebJob{
+    public class Jobs{
+        public async Task WaitForTimer([TimerTrigger("*/5 * * * * *", RunOnStartup = true)]
+            TimerInfo timerInfo, TextWriter log) {
+            await log.WriteAsync($"C# Timer trigger function executed at: {DateTime.Now} ");
+        }
+    }
+}
---
+++ WebJob/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using Core.Logging;
+using Core.Logging.AppInsights;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace WebJob{
+    class Program{
+        public static IConfiguration Configuration { get; set; }
+
+        static void Main(string[] args){
+            Console.WriteLine("Host starting up");
+            ReadConfigurationFromSettings();
+            PrepareApplicationLogger();
+            StartWebJobHost();
+        }
+
+        private static void PrepareApplicationLogger(){
+            var appInsightsInstrumentationKey = Configuration["AppInsightsKey"];
+            var submitter = new AppInsightsEventSubmitter(appInsightsInstrumentationKey);
+            Insights.Tracker.DefaultEventBuilder.AddSubmitter(submitter);
+            Insights.Tracker.DefaultEventBuilder
+                .ToApplication("FleetBack");
+        }
+
+        private static void StartWebJobHost(){
+            var config = new JobHostConfiguration();
+            config.LoggerFactory = new LoggerFactory().AddConsole();
+            config.UseTimers();
+            var host = new JobHost(config);
+            host.RunAndBlock();
+        }
+
+        private static void ReadConfigurationFromSettings(){
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            Configuration = builder.Build();
+        }
+    }
+}
